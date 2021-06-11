a44e096042a35537fe1fac0d7fe7eb657f12a7cd
NekoMt.Tai <liuyibo97@outlook.com>
2021-06-11 12:19:41 +0800

    Change tmp folder name
    To avoid conflict with the project folder name

--- gdriveget.sh
+++ gdriveget.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-mkdir gdriveget
+mkdir gdriveget_tmp
 SLNK=$1
 FNAME=$2
 FILEID=${SLNK/"https://drive.google.com/file/d/"/""}
 FILEID=${FILEID/"/view?usp=sharing"/""}
-wget --quiet --save-cookies gdriveget/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILEID}" -O $FNAME
+wget --quiet --save-cookies gdriveget_tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILEID}" -O $FNAME
 HEAD=$(cat $FNAME | head -n 1)
 TAIL=$(cat $FNAME | tail -n 1)
 if [[ $(echo $HEAD | grep "Google Drive - Virus scan warning") != "" ]] && [[ $(echo $TAIL | grep $FILEID) != "" ]]
 then
-    COOKIEFILE=$(cat gdriveget/cookies.txt)
+    COOKIEFILE=$(cat gdriveget_tmp/cookies.txt)
     CODE=${COOKIEFILE:0-4:4}
-    wget --load-cookies gdriveget/cookies.txt "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILEID}" -O $FNAME
+    wget --load-cookies gdriveget_tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILEID}" -O $FNAME
 fi
-rm -rf gdriveget
+rm -rf gdriveget_tmp
